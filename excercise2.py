82419183c2cd1eebba0a2ffb56619f01272c0667
Gary Blydenburgh <gary@blydenburgh.com>
2026-09-18 06:02:47 +0000

    Remove unnecessary blank line in run_idempotency_test function

--- excercise2.py
+++ excercise2.py
@@ -172,7 +172,6 @@ def run_idempotency_test() -> None:
     partial_apply_merged_state = build_merged_state(partial_apply_have_by_id, want_by_id)
     expected_partial_apply_rendered_cmds = ['vlan 60', ' name PRINTERS']
     
-
     
     for test_name, effective_state, have_by_id, expected_result in [
         (
