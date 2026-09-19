5b35b48dc2a04ca7098ca0933ecd01c29dcaebba
Gary Blydenburgh <gary@blydenburgh.com>
2026-09-19 15:09:31 +0000

    Remove trailing whitespace in idempotency test for improved clarity

--- excercise2.py
+++ excercise2.py
@@ -171,7 +171,7 @@ def run_idempotency_test() -> None:
     partial_apply_have_by_id = index_vlan_data(partial_apply_parsed)
     partial_apply_merged_state = build_merged_state(partial_apply_have_by_id, want_by_id)
     expected_partial_apply_rendered_cmds = ['vlan 60', ' name PRINTERS']
-        
+    
     for test_name, effective_state, have_by_id, expected_result in [
         (
             "IDEMPOTENCY FIRST PASS TEST",
